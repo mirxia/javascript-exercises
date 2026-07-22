0d5fc41afb363772d642ebe183768fa654e48893
mirxia <3803518+mirxia@users.noreply.github.com>
2026-07-22 10:29:16 +0800

    Completed 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempFahrenheit = Math.round(((temp - 32) * 5 / 9) * 10) / 10;
+  return tempFahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempCelsius = Math.round((temp * 9 / 5 + 32) * 10) / 10;
+  return tempCelsius;
 };
 
 // Do not edit below this line
