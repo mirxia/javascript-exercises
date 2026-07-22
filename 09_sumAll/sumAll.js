0487860c8b5fdc2d815d8043f5cdb19d6d69d636
mirxia <3803518+mirxia@users.noreply.github.com>
2026-07-22 09:49:55 +0800

    Completed 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const { error } = require("selenium-webdriver");
 
+const sumAll = function(a, b) {
+    for (let i = 0; i < arguments.length; i++) {
+        if (Number.isInteger(arguments[i]) === false || arguments[i] < 0) {
+            return "ERROR";
+        }
+    }
+    const args = Array.from(arguments).sort((a,b) => a - b);
+    let sum = 0;
+    for (let i = args[0]; i <= args[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
