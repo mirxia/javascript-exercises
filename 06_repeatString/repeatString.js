5693c46df79713f89f1cdab488a30e41a3abe7c8
mirxia <3803518+mirxia@users.noreply.github.com>
2026-07-22 08:58:18 +0800

    Completed 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let repeatedStr = '';
+    for (let i = 0; i < num; i++) {
+        repeatedStr += str
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
